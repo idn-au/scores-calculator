facf0984c4e2cf43a766c6d60263f3ccc1fe0e9f
jamiefeiss <jamie.feiss@gmail.com>
2026-03-24 09:50:11 +1000

    Fixed nested tables in README, small docs updates

--- yaml-to-markdown/convert.py
+++ yaml-to-markdown/convert.py
@@ -9,13 +9,15 @@ YAML_DIR = PROJECT_DIR.parent / "definitions"
 def render_requirements(contents: list) -> str:
     render_str = "<table>"
     for req in contents:
-        render_str += f"""
-            <tr>
-                <td>{req["description"]} [{req["value"]}]</td>
-                {f"<td {'colspan="2"' if not req.get('conditions') else ''}><p>Query</p><pre>{req['query']}</pre></td>" if req.get("query") else ""}
-                {f"<td {'colspan="2"' if not req.get('query') else ''}><p>Conditions</p><ul>{''.join([f'<li>{cond["title"]}</li>' for cond in req['conditions']])}</ul></td>" if req.get("conditions") else ""}
-            </tr>
-        """
+        render_str += f"""<tr>
+                <td>{req["description"]} [{req["value"]}]</td>"""
+        if req.get("query"):
+            render_str += f"""
+                <td {'colspan="2"' if not req.get('conditions') else ''}><p>Query</p><pre>{req['query'].replace('<', '&lt;').replace('>', '&gt;')}</pre></td>"""
+        if req.get("conditions"):
+            render_str += f"""
+            <td {'colspan="2"' if not req.get('query') else ''}><p>Conditions</p><ul>{''.join([f'<li>{cond["title"]}</li>' for cond in req['conditions']])}</ul></td>"""
+        render_str += "</tr>"
     render_str += "</table>"
     return render_str
 
@@ -23,15 +25,13 @@ def render_requirements(contents: list) -> str:
 def render_scores(contents: dict) -> str:
     render_str = "<table>"
     for key, value in contents.items():
-        render_str += f"""
-            <tr>
-                <td>
-                    <p>{key.upper() if key.upper() == value["title"] else f"{key.upper()} - {value['title']}"}</p>
-                    <p><em>{value["description"]}</em></p>
-                    {render_scores(value["scores"]) if value.get("scores") else render_requirements(value["requirements"])}
-                </td>
-            </tr>
-        """
+        render_str += f"""<tr>
+            <td>
+                <p>{key.upper() if key.upper() == value["title"] else f"{key.upper()} - {value['title']}"}</p>
+                <p><em>{value["description"]}</em></p>
+                {render_scores(value["scores"]) if value.get("scores") else render_requirements(value["requirements"])}
+            </td>
+        </tr>"""
     render_str += "</table>"
     return render_str
 
@@ -44,7 +44,7 @@ def main():
             render_str += f"<p>{f.stem.split('Def')[0].upper()}</p>"
             render_str += render_scores(contents)
     print(render_str)
-    with open(PROJECT_DIR.parent / "test2.html", "w") as f:
+    with open(PROJECT_DIR.parent / "test.html", "w") as f:
         f.write(render_str)
 
 
